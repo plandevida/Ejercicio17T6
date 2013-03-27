64119e1223036e62925d03c282ab5cca5b9eafb5
plandevida <plandevida@planes.com>
2013-03-27 14:42:39 +0100

    cabeceras añadidas

--- Ejercicio17T6/main.cpp
+++ Ejercicio17T6/main.cpp
@@ -7,12 +7,13 @@
 //
 
 #include <iostream>
+#include "Lista.h"
 
-int main(int argc, const char * argv[])
-{
 
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main(int argc, const char * argv[]) {
+    
+    
+    
     return 0;
 }
 
